a4b45010b7652ac64d3fa119beb390728a2febdf
CodingCocho <raulujr@gmail.com>
2022-11-10 22:36:11 -0800

    Created UI.

---
+++ ui/src/assets/chars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="chars" tilewidth="16" tileheight="16" tilecount="12544" columns="128">
+ <image source="../../../../../../../Downloads/Character v.2/Character v.2/characters/char_all.png" width="2048" height="1568"/>
+</tileset>
---
+++ ui/src/assets/floral outfit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="floral outfit" tilewidth="16" tileheight="16" tilecount="15680" columns="160">
+ <image source="../../../../../../../Downloads/Character v.2/Character v.2/clothes/floral.png" width="2560" height="1568"/>
+</tileset>
